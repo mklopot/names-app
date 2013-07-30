073feefb7d21a5310cf7139a8c851dc44067651d
Mike Klopot <michael.klopot@gmail.com>
2013-07-29 22:16:59 -0700

    refactor to get rid of some globals

--- names-app.py
+++ names-app.py
@@ -10,25 +10,19 @@ import logging
 LOGFORMAT = "%(levelname)s %(funcName)s: %(message)s"
 logging.basicConfig(stream=sys.stderr, level=logging.INFO, format=LOGFORMAT)
 
-global progress_max
-global top
 global replay_sequence
 replay_sequence = []
-top = 8
 
 def loadprofile(profile="default"):
     profile = shelve.open(profile, writeback=True) 
     if "initdataset" in profile:
         print "Loading saved profile..."
-        global progress_max
         global progress_points
         global replay_sequence
-        global top
         replay_sequence = copy.copy(profile["saved_sequence"])
         logging.debug("Loading saved sequence into replay sequence " + str(replay_sequence))
-        progress_max = profile["progress_max"]
         progress_points = profile["progress_points"]
-        top = profile["top"]
+  
         return profile["initdataset"], profile
     else:
         print "Saved profile not found, initializing..."
@@ -36,27 +30,25 @@ def loadprofile(profile="default"):
    
 
 def createprofile(profile="default", filename="names.txt"):
-    global progress_max
     global progress_points
     global replay_sequence
+
     f = open(filename)
     names = f.readlines()
     random.shuffle(names)
-
-
+    names = map(lambda x: x.rstrip(), names)
+    profile["top"] = 10
     profile["progress_points"] = 0
-    progress_points = profile["progress_points"]
     
     profile["initdataset"] = names
     profile["saved_sequence"] = []
     logging.debug("Initializing replay sequence")
-    profile["top"] = top
+
 
     length = len(names)
     k = math.floor(math.log(length,2)) + 1
     profile["progress_max"] = int(1 + k * length - 2 ** k)
     logging.debug("Worst-case number of comparisons: " + str(profile["progress_max"]))
-    progress_max = profile["progress_max"] 
     profile.sync()
     print "Created a profile to select top " + str(top) + " out of " + str(length) + " names"
     
@@ -70,12 +62,12 @@ def replay():
     else:
         yield None
 
-def ask_user(a, b, profile=None):
+def ask_user(a, b, profile):
     global progress_points
     print
     print 17 * "_"
     print
-    print "Progress: " + str(profile["progress_points"]) + " of less than " + str(progress_max)
+    print "Progress: " + str(profile["progress_points"]) + " of less than " + str(profile["progress_max"])
     print "Which of these names do you like better?\nPress (1) or (2)\n1. " + a + "\n2. " + b
     i=0
     i = next(replay())
@@ -88,17 +80,16 @@ def ask_user(a, b, profile=None):
         i = raw_input('>')
         while i not in ["1", "2"]:
             i = raw_input('>')
-        if profile:
-            
-            logging.debug("Saving your selection in the database")
-            profile["saved_sequence"].append(i)
-            logging.debug("Database replay sequence is now" + str(profile["saved_sequence"]))
-            profile["progress_points"] += 1
-            profile.sync()
+
+        logging.debug("Saving your selection in the database")
+        profile["saved_sequence"].append(i)
+        logging.debug("Database replay sequence is now" + str(profile["saved_sequence"]))
+        profile["progress_points"] += 1
+        profile.sync()
     return int(i) - 1
 
 
-def merge(left, right, profile=None):
+def merge(left, right, profile):
     result = []
     i, j = 0, 0
     while i < len(left) and j < len(right):
@@ -114,22 +105,22 @@ def merge(left, right, profile=None):
     points += len(right) - j - 1
     if points > 0:
         print "BONUS!!! " + str(points) + " Progress Points!!!"
-    progress_max -= points
+    profile["progress_max"] -= points
 
     result += left[i:]
     result += right[j:]
     
     return result
 
-def mergesort(lst, profile=None):
+def mergesort(lst, profile):
     if len(lst) <= 1:
         return lst
     middle = int(len(lst) / 2)
     left = mergesort(lst[:middle],profile)
     right = mergesort(lst[middle:],profile)
     result = merge(left, right, profile)
-    if len(result) > top:
-        result = result[-top:]
+    if len(result) > profile["top"]:
+        result = result[-profile["top"]:]
     return result
 
 if __name__ == "__main__":
